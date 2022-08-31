b62e7da57f42aab0ef25d17c119baf0aff933650
Rizky Adil Hakim <45657521+kimsohyuncans@users.noreply.github.com>
2022-08-31 15:10:32 +0700

    Update server.go

--- server.go
+++ server.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	app.Get("/dsjfksjfkl", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
